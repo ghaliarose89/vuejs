1bc8b577a7639876f3e592897219f1f6f7aec6c4
unknown <ghaliarose89@gmail.com>
2022-04-03 14:00:52 -0600

    renderning tasks

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button :style="{background:color}"   @click="onclick()"  class="btn" >{{text}}</button>
+</template>
+
+<script>
+export default {
+    name:"Button",
+    props:{
+        text:String,
+        color:String,
+    },
+
+methods:{
+    onclick(){
+        console.log("clciked")
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
